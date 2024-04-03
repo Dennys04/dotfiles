1dd515ec66eb0c71874985a9efea2a30907c5e46
Dennys Morillo <dennysmq04@gmail.com>
2024-04-03 20:33:48 +0200

    git aliases added

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,21 @@ end
 
 # Aliases for Kitty Terminal
 alias icat="kitten icat"
+
+# Aliases Git
+alias gti="git init"
+alias gts="git status"
+alias gtc="git commit"
+alias gtps="git push"
+alias gta="git add"
+
+# Environment Variables
+
+# JAVA
+
+set -x JAVA_HOME $HOME/.jdks/jdk17
+set -x PATH $JAVA_HOME/bin $PATH
+
+# Rust pkgs via Cargo
+
+set -x PATH ~/.cargo/bin/ $PATH
